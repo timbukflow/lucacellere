0635e26ac44e9f4be24ab0187d0f7cee7ecdc6bb
timbuk <ivoschwizer@gmail.com>
2019-11-26 07:25:12 +0100

    scrollin nav

--- js/main.js
+++ js/main.js
@@ -4,20 +4,24 @@ $(document).ready(function () {
     
     $('.navinfo').click(function() {
         $('.navinfocontainer').css('left','0px');
+        $('body').addClass('respfix');
     });
     
     $('.closernav').click(function() {
         $('.navinfocontainer').css('left','-100%');
+        $('body').removeClass('respfix')
     });
     
     $('.open').click(function() {
         var navinfo = $(this).attr("data-navinfo");
         $(navinfo).css('left','0%');
+        $('body').addClass('respfixinfo')
     });
     
     $('.closer').click(function() {
         var closer = $(this).attr("data-closer");
         $(closer).css('left','100%');
+        $('body').removeClass('respfixinfo')
     });
     
     $('.impressum').click(function() {
